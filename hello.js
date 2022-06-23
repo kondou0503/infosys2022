ebe63394860284867f3a4de5c675285478902dba
近藤七海 <nanami@kondounanaminoMacBook-Pro-3.local>
2022-06-23 14:16:31 +0900

    0623

---
+++ hello.js
@@ -0,0 +1,25 @@
+// alert("hello, world");
+
+// var username;
+// username = prompt("あなたのお名前は？");
+// alert("Hello, " + username);
+
+// var Number, sum;
+// sum = 0;
+// num = prompt("数値を入れてください。0にすると終了します。");
+// while (num != 0) {
+//     sum = sum + parseInt(num)
+//     num = prompt("数値を入れてください。0にすると終了します。");
+// }
+// alert("総和は " + sum);
+
+var max, num
+max = 0
+num = prompt("新しい数値を入れてください。0にすると終了します。");
+while (num != 0){
+    if (parseInt(num) > max){
+        max = num;
+    }
+    num = prompt("新しい数値を入れてください。0にすると終了します。");
+}
+alert("最大値は " + max);
