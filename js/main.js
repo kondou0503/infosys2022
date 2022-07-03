e7a25af9860f426c14b12edbc210e04fb27e8297
近藤七海 <nanami@kondounanaminoMacBook-Pro-3.local>
2022-07-03 20:37:35 +0900

    2022/07/03

--- js/main.js
+++ js/main.js
@@ -1,26 +1,23 @@
-'use strict';
+'use strict'
 
 {
-    const btn = document.getElementById('btn');
+  const btn = document. getElementById('btn');
 
-    btn.addEventListener('click', () => {
-        // btn.textContent = 'hit!';
-        const results = ['大吉', '中吉', '凶', '末吉'];
-        const n = Math.floor(Math.random() * results.length);
+  btn.addEventListener('click' , () => {
+    const results = ['大吉', '中吉', '凶'];
+    const n = Math.floor(Math.random() * 3);
+    btn.textContent = results[n]
 
-        btn,rextContent = results[n];
-
-        // switch(n){
-        //     case 0:
-        //         btn.textContent = '大吉'
-        //         break;
-        //     case 1:
-        //         btn.textContent = '中吉'
-        //         break;
-        //     case 2:
-        //         btn.textContent = '凶'
-        //         break;
-        //     }
-
-    });
+//     switch (n) {
+//       case 0:
+//         btn.textContent = '大吉';
+//         break;
+//       case 1:
+//         btn.textContent = '中吉';
+//         break;
+//       case 2:
+//         btn.textContent = '凶';
+//         break;
+//     }
+});
 }
